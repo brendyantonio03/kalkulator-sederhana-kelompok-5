93a0d171a251821ad0a3d64f50ac95cbe5ef0739
Brendy Antonio <brendyya526@gmail.com>
2023-10-14 00:07:50 +0700

    first commit

---
+++ test/prj_kalkulator_sederhana_test.dart
@@ -0,0 +1,8 @@
+import 'package:prj_kalkulator_sederhana/prj_kalkulator_sederhana.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
